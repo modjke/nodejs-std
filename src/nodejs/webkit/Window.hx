a4995f1abd412f64fd672e5e78c196f41d36b6a0
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2015-01-14 15:48:50 +0300

    Window.isDevToolsOpen

--- src/nodejs/webkit/Window.hx
+++ src/nodejs/webkit/Window.hx
@@ -31,7 +31,9 @@ extern class Window {
 	public function showDevTools( ?id : String, ?headless : Bool ) : Void;
 	public function closeDevTools() : Void;
 	public function enterKioskMode() : Void;
-	public function leaveKioskMode(): Void;
+	public function leaveKioskMode(): Void;	
+	public function capturePage(callback:String->Void):Void;
+	public function isDevToolsOpen():Bool;
 	
 	@:overload(function( event : String, callb : Dynamic -> Dynamic ) : Void {})
 	@:overload(function( event : String, callb : Void -> Dynamic ) : Void {})
